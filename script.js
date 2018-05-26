d57858b0575b1b901ee3015eeb2fbdc54a41c573
Unknown <marcelo.guzman9@yahoo.com>
2018-05-25 18:28:30 -0700

    Created index.html&script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+class Employee {
+    constructor(name, salary, hireDate) {
+      this.name = name;
+      this.salary = salary;
+      this.hireDate = hireDate;
+    }
+    getName() {
+      console.log(this.name.toUpperCase());
+    }
+    getSalary() {
+      console.log(this.salary);
+    }
+    getHireDate() {
+      console.log(this.hireDate);
+    }
+  }
