f0e3eb5171d078059c7048f5f45f861dd0343dee
Unknown <marcelo.guzman9@yahoo.com>
2018-05-26 00:10:32 -0700

    started coding on script.js

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ class Employee {
       console.log(this.hireDate);
     }
   }
+class Manager extends Employee {
+  constructor(jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate ) {
+     super (name, salary, hireDate);
+     this.jobDescription = jobDescription;
+     this.yearsExperience = yearsExperience;
+     this.degreeCompleted = degreeCompleted;
+
+ 
+     
+
+
+     
+
+                
+
+                }
+
+}
+// constructor (constructer is (jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate))
+  // class Person {
+  //constructor(firstName, lastName) {
+    //this.firstName = firstName;
+    //this.lastName = lastName;
+  //}
+
+  //greeting() {
+  //  console.log("Hello, my name is " + this.firstName + ".");
+ // }
+//}
+
+//class NewStudent extends Person {
+  //constructor(studentID, firstName, lastName) {
+    //super(firstName, lastName);
+    //this.studentID = studentID;
+  //}
+  //studentInfo() {
+    //console.log(this.studentID + " " + this.firstName + ", " + this.lastName);
+  //}
+//}
+
+//let student = new NewStudent(12, "John", "Smith");
+//student.greeting();
+//student.studentInfo();
