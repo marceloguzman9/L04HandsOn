64e8b2afe78c888de2ee98c832e93bf1829e9e02
Unknown <marcelo.guzman9@yahoo.com>
2018-05-26 21:20:58 -0700

    Added subClass Designer in js

--- script.js
+++ script.js
@@ -1,73 +1,72 @@
 class Employee {
-    constructor(name, salary, hireDate) {
-      this.name = name;
-      this.salary = salary;
-      this.hireDate = hireDate;
-    }
-    getName() {
-      console.log(this.name.toUpperCase());
-    }
-    getSalary() {
-      console.log(this.salary);
-    }
-    getHireDate() {
-      console.log(this.hireDate);
-    }
+  constructor(name, salary, hireDate) {
+    this.name = name;
+    this.salary = salary;
+    this.hireDate = hireDate;
   }
+  getName() {
+    console.log(this.name.toUpperCase());
+  }
+  getSalary() {
+    console.log(this.salary);
+  }
+  getHireDate() {
+    console.log(this.hireDate);
+  }
+}
 
 class Manager extends Employee {
-  constructor(jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate, managedEmps, xActivities ) {
+constructor(jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate, managedEmps) {
+  super (name, salary, hireDate);
+  this.jobDescription = jobDescription;
+  this.yearsExperience = yearsExperience;
+  this.degreeCompleted = degreeCompleted;
+  this.managedEmps= managedEmps;
+  
+}
+getJobDescription() {
+  console.log(this.jobDescription);
+}
+getManagedEmps() {
+  console.log(this.managedEmps);
+}
+}
+let shiftMgr = new Manager("Manages people", "4 years", "yes", "DJ Brooks", "$50,00", "1/1/2018", "9");
+shiftMgr.getName();
+shiftMgr.getJobDescription();
+shiftMgr.getHireDate();
+shiftMgr.getManagedEmps();
+
+
+class Designer extends Employee {
+  constructor(jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate) {
     super (name, salary, hireDate);
     this.jobDescription = jobDescription;
     this.yearsExperience = yearsExperience;
     this.degreeCompleted = degreeCompleted;
-    this.managedEmps= managedEmps;
-    this.xActivities= xActivities;
   }
-  getJobDescription() {
+  getYearsExperience() {
+    console.log(this.yearsExperience);
+  }
+  getDegreeCompleted() {
     console.log(this.jobDescription);
   }
-  getManagedEmps() {
-    console.log(this.managedEmps);
-  }
-  getxActivities() {
-    console.log(this.xActivities);
-  }
-
-  
 }
+let oldDsgr= new Designer("Creates Clothing", "3 years", "yes", "Calvin DaKine", "$45,000", "3/4/2018");
+oldDsgr.getName();
+oldDsgr.getHireDate();
+oldDsgr.getSalary();
+oldDsgr.getYearsExperience();
   
- class Designer extends Employee {
-  constructor(jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate ) {
+
+class SalesAssociate extends Employee {
+    constructor(jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate ) {
     super (name, salary, hireDate);
     this.jobDescription = jobDescription;
     this.yearsExperience = yearsExperience;
     this.degreeCompleted = degreeCompleted;
-  }
-}
-  
-  class SalesAssociate extends Employee {
-    constructor(jobDescription, yearsExperience, degreeCompleted, name, salary, hireDate ) {
-      super (name, salary, hireDate);
-      this.jobDescription = jobDescription;
-      this.yearsExperience = yearsExperience;
-      this.degreeCompleted = degreeCompleted;
     }
 }   
 
-let shiftMgr = new Manager(
-    "Manages people"
-    , "4 years"
-    , "yes"
-    , "DJ Brooks"
-    , "$50,00"
-    , "1/1/2018"
-    , "9"
-    , "Mentor/Teacher on JavaScript"
-);
 
-shiftMgr.getJobDescription();
-shiftMgr.getHireDate();
-shiftMgr.getName();
-shiftMgr.getManagedEmps();
-shiftMgr.getxActivities();
+
